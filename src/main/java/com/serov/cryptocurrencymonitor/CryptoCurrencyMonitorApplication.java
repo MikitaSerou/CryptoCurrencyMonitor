cd7e23f2390f82e2bd3c909a7a87c48de9b8ad6c
Серов Никита Вадимович <serov@nca.by>
2023-05-15 14:44:38 +0300

    Init

---
+++ src/main/java/com/serov/cryptocurrencymonitor/CryptoCurrencyMonitorApplication.java
@@ -0,0 +1,13 @@
+package com.serov.cryptocurrencymonitor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CryptoCurrencyMonitorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CryptoCurrencyMonitorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/serov/cryptocurrencymonitor/CryptoCurrencyMonitorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.serov.cryptocurrencymonitor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CryptoCurrencyMonitorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
