0470ea3aaac1c85de87330b209a525665736ce64
MikitaSerou <miktia.serou@gmail.com>
2023-05-16 22:48:03 +0300

    Models description in OpenAPI

--- src/main/java/com/serov/cryptocurrencymonitor/payload/response/ErrorResponse.java
+++ src/main/java/com/serov/cryptocurrencymonitor/payload/response/ErrorResponse.java
@@ -1,4 +1,10 @@
 package com.serov.cryptocurrencymonitor.payload.response;
 
-public record ErrorResponse(String errorName, String message) {
+import io.swagger.v3.oas.annotations.media.Schema;
+
+@Schema(description = "The object returned in case of an exception")
+public record ErrorResponse(@Schema(description = "Short internal error name for developers")
+                            String errorName,
+                            @Schema(description = "Short description of the error with its cause")
+                            String message) {
 }
