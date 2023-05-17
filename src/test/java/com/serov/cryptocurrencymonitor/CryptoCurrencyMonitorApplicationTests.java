a62a4a3e3f9c4c5a33913c3d7c104ba0f5f20e03
Серов Никита Вадимович <serov@nca.by>
2023-05-17 19:24:54 +0300

    Subscriptio service test, tests naming

--- src/test/java/com/serov/cryptocurrencymonitor/CryptoCurrencyMonitorApplicationTests.java
+++ src/test/java/com/serov/cryptocurrencymonitor/CryptoCurrencyMonitorApplicationTests.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@DisplayName("Context test")
 class CryptoCurrencyMonitorApplicationTests {
 
     @Test
