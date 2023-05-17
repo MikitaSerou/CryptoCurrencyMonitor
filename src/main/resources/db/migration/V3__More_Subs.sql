c8dd70f2be5ba52c0dd5c87ad6987a470ee90e77
MikitaSerou <miktia.serou@gmail.com>
2023-05-17 22:06:07 +0300

    Migration with more subscriptions for see difference

---
+++ src/main/resources/db/migration/V3__More_Subs.sql
@@ -0,0 +1,9 @@
+INSERT INTO subscriptions (subscribed_price, username, crypto_currency_id)
+VALUES
+    (26045.77, 'Gleb', 90),
+    (1720.11, 'Mila', 80),
+    (1800.76, 'Elvira', 80),
+    (1820.76, 'Mikhail', 80),
+    (27117.3, 'Katya', 90),
+    (19.91, 'Mikhail', 48543),
+    (1835.76, 'Olga', 80);
